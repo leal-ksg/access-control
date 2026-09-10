630c9370c345fe44e968def0b5a5d272275e480b
leal-ksg <kauagsilvaleal@gmail.com>
2026-09-09 22:56:14 -0300

    wip: initial mqtt communication

---
+++ backend/src/mqtt/config.ts
@@ -0,0 +1,30 @@
+import mqtt, { IClientOptions } from "mqtt";
+
+const brokerUrl = process.env.MQTT_BROKER_URL || "mqtt://192.168.1.81:1883";
+const options: IClientOptions = {
+  username: process.env.MQTT_BROKER_USERNAME || "dev_mqtt_user",
+  password: process.env.MQTT_BROKER_PASSWORD || "dev_mqtt_password",
+};
+
+console.log(process.env.MQTT_BROKER_USERNAME);
+console.log(process.env.MQTT_BROKER_PASSWORD);
+console.log(process.env.MQTT_BROKER_URL);
+
+const mqttClient = mqtt.connect(brokerUrl, options);
+
+mqttClient.on("connect", () => {
+  console.log("-> Broker MQTT conectado com sucesso!");
+
+  mqttClient.subscribe("teste");
+});
+
+mqttClient.on("error", (err) => {
+  console.log("-Erro no Broker MQTT:", err);
+});
+
+mqttClient.on("message", (topic, message) => {
+  console.log(topic);
+  console.log(message.toString());
+});
+
+export default mqttClient;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,15 @@
+import "dotenv/config";
+import "./mqtt/config";
+
 import cookieParser from "cookie-parser";
 import express from "express";
-import dotenv from "dotenv";
+
 import cors from "cors";
 import { connectDB } from "./database/prisma";
 import { withRetry } from "./core/with-retry";
 import { errorHandlerMiddleware } from "./middlewares/error-handler-middleware";
 
 async function main() {
-  dotenv.config();
-
   await withRetry(connectDB);
 
   const PORT = process.env.PORT || 3001;
@@ -22,7 +23,7 @@ async function main() {
   server.use(express.json());
   // server.use("/api/v1", router);
 
-  server.use(errorHandlerMiddleware)
+  server.use(errorHandlerMiddleware);
 
   server.listen(PORT, () => {
     console.log(`-> Server is running on port ${PORT}`);
